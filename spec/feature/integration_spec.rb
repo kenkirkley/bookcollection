1d91a37c877d962860d3b756f48ed2d2b37c909e
root <root@localhost>
2022-02-04 00:34:51 +0000

    Addition of attributes to book

---
+++ spec/feature/integration_spec.rb
@@ -0,0 +1,21 @@
+# location: spec/feature/integration_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'Creating a book', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'J.K. Rowling'
+    fill_in 'Price', with: 10.00
+    select '2022', :from => 'book_published_date_1i'
+    select 'January', :from => 'book_published_date_2i'
+    select '1', :from => 'book_published_date_3i'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('harry potter')
+    expect(page).to have_content('J.K. Rowling')
+    expect(page).to have_content('10.0')
+    expect(page).to have_content('2022-01-01')
+  end
+end
+
